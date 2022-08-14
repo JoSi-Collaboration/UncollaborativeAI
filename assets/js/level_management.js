eaf7d3c175bb7a8edf434f3ec0cd39e422c4bde7
Pythonwolf7734 <82569523+Pythonwolf7734@users.noreply.github.com>
2022-08-14 19:06:48 +0200

    fixed condition & added time's up

--- assets/js/level_management.js
+++ assets/js/level_management.js
@@ -13,47 +13,31 @@ levels = {
             ai_click();
         }, 5000);`,
         "html": "",
-        "tip": "You need to press the Player button at the same time as the AI presses the AI button"
+        "tips": ["You need to press the Player button at the same time as the AI presses the AI button"]
     },
     "2": {
-        "js": `function StartAI(){
-            var id = window.setInterval(function() {
-                if (current_lvl != 1) {
-                    window.clearInterval(id);
-                }
-                ai_click();
-            }, 5000);
-        }
-        function changeTip(){
-            var newTip = "Maybe you may need to change the code."
-        }
-        
-        setTimeout(function() { changeTip(); }, 5000);`,
+        "js": ``,
         "html": `
-        <td class="half - w nb button - td "> 
-            <button disabled type = "button" class = "hidden" id = "aienable" onclick="window.setInterval(function() {ai_click();}, 5000);")>Start AI</button> 
-        </td>`,
-        "tip": "You need to find the button to enable the AI or somehow else enable the AI"
+            <button type="button" class="hidden player_button" id="aienable" onclick="var id = window.setInterval(function() {if (current_lvl != 2) {window.clearInterval(id);return;};ai_click();}, 5000);document.getElementById('aienable').remove();")>Start AI</button> 
+        `,
+        "tips": ["You need to find the hidden button to enable the AI or somehow else enable the AI", "Maybe you may need to change the code."]
     }
 };
 
 var setInnerHTML = function(elm, html) {
     elm.innerHTML = html;
-    Array.from(elm.querySelectorAll("script")).forEach(oldScript => {
-        const newScript = document.createElement("script");
-        Array.from(oldScript.attributes)
-            .forEach(attr => newScript.setAttribute(attr.name, attr.value));
-        newScript.appendChild(document.createTextNode(oldScript.innerHTML));
-        oldScript.parentNode.replaceChild(newScript, oldScript);
+    Array.from(elm.querySelectorAll("script")).forEach( oldScript => {
+      const newScript = document.createElement("script");
+      Array.from(oldScript.attributes)
+        .forEach( attr => newScript.setAttribute(attr.name, attr.value) );
+      newScript.appendChild(document.createTextNode(oldScript.innerHTML));
+      oldScript.parentNode.replaceChild(newScript, oldScript);
     });
 }
 
 function player_click() {
     let now = Math.floor(Date.now() / 1000);
-    //console.log(now - last_lvl_upd);
-    //console.log(now - last_ai_click);
-
-    if (now - last_ai_click > 0.3 || now - last_lvl_upd < 0.3) {
+    if (now-last_ai_click > 0.3 || now-last_lvl_upd < 0.3){
         return
     } else {
         next_level();
@@ -64,10 +48,10 @@ function ai_click() {
     last_ai_click = Math.floor(Date.now() / 1000);
     btn = document.getElementById("ai");
     btn.style.backgroundColor = "green";
-    (async() => {
+    (async()=> {
         await new Promise(r => setTimeout(r, 300));
         btn.style.backgroundColor = "#8a2dfcfd";
-    })()
+    }) ()
 }
 
 function next_level() {
@@ -76,6 +60,7 @@ function next_level() {
         "min": 5,
         "sec": 0
     }
+    let unnes = document.getElementById("unneccessary")
     let main_area = document.getElementById("content")
     let code_area = document.getElementById("js_loading");
     current_lvl++;
@@ -101,16 +86,29 @@ function next_level() {
     }
     time_update = window.setInterval(function() {
         if (time["sec"] == 0) {
-            time["min"] = time["min"] - 1;
+            time["min"] = time["min"]-1;
             time["sec"] = 59;
         } else {
-            time["sec"] = time["sec"] - 1;
+            time["sec"] = time["sec"]-1;
         }
         document.getElementById("time").innerHTML = `${time["min"]}min ${time["sec"]}sec`;
+        if (time["sec"] == 0 && time["min"] == 0) {
+            code_area.innerHTML = "";
+            unnes.innerHTML = "";
+            main_area.innerHTML = `
+            <div id="start">
+                <h1>Time's up</h1><br>
+                <h3>You ran out of time. You have successfully solved ${current_lvl-1} levels!</h3><br>
+            </div>
+            `;
+            window.clearInterval(time_update)
+        }
     }, 1000);
     if (!(String(current_lvl) in levels)) {
-        current_lvl = current_lvl - 1;
+        window.clearInterval(time_update)
+        current_lvl = current_lvl-1;
         code_area.innerHTML = "";
+        unnes.innerHTML = "";
         confetti({
             particleCount: 500,
             spread: 100,
@@ -128,8 +126,7 @@ function next_level() {
     js = levels[String(current_lvl)]["js"];
     html = levels[String(current_lvl)]["html"];
     setInnerHTML(code_area, `<script>${js}</script>`);
-    unnes = document.getElementById("unneccessary")
-        /*const lines = str.split(/\r\n|\r|\n/);*/
+    /*const lines = str.split(/\r\n|\r|\n/);*/
     unnes.innerHTML = `${unnes.innerHTML}
     ${html}`;
 }
